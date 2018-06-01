4b2d9a03b0c33068293e3c7cbf1456c3304fec06
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-06-01 08:12:11 -0500

    Initial Commit

---
+++ BunsanMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BunsanMessenger
+//
+//  Created by Misael Pérez Chamorro on 6/1/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
