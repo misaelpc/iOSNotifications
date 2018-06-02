dce216fab996b68ecce45b7f690eb1d40713125d
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-06-01 21:09:15 -0500

    Bind Table View with Data

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -10,18 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
   let messagesStore = MessagesStore.sharedInstance
+  @IBOutlet weak var tableView: UITableView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     messagesStore.fetchMessagesFromJson()
+    setupTableViewCell()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  func setupTableViewCell() {
+    self.tableView.register(UINib(nibName: "MessagesTableViewCell",
+                                  bundle: nil), forCellReuseIdentifier: "MessagesTableViewCell")
+  }
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
+}
 
+extension ViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return messagesStore.messages.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MessagesTableViewCell", for: indexPath) as? MessagesTableViewCell
+    return cell!
+  }
 }
 
